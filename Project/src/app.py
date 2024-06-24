322e3cbbae2bcae1447ae9015e67f0179c62c4d5
lahirumenik <115687865+lahirumenik@users.noreply.github.com>
2024-06-24 16:16:21 +0530

    Prediction in flask - complete - working correctly

--- Project/src/app.py
+++ Project/src/app.py
@@ -4,40 +4,64 @@ from src.components.data_ingestion import remove_outliers_iqr
 from src.components.data_ingestion import calculate_metrics
 from src.pipeline.predict_pipeline import predict
 import pandas as pd
+from collections import defaultdict
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+df = pd.read_csv('artifacts/dataset.csv')
+person1 = df[df['ClassLabel'] == 1]
+person2 = df[df['ClassLabel'] == 2]
+person3 = df[df['ClassLabel'] == 2]
+person4 = df[df['ClassLabel'] == 4]
+person5 = df[df['ClassLabel'] == 5]
+
+personas = [person1, person2, person3, person4, person5]
+
+# for index, person_df in enumerate(personas):
+#     # Remove outliers for each column
+#     for col in ['X', 'Y', 'Z', 'Mixed']:
+#         df[df['ClassLabel']==index+1] = remove_outliers_iqr(person_df, col)
+
 @app.route('/data')
 def data():
-    # Generate random data
-    x = np.linspace(0, 100, 100)
-    y1 = np.random.rand(100).tolist()
-    y1 = y1*0.2
-    y2 = np.random.rand(100).tolist()
-    y2=y2*0.4
-    y3 = np.random.rand(100).tolist()
-    y3=y3*0.4
-    y4 = np.random.rand(100).tolist()
-    y4=y4*0.6
+    target_idx = np.random.randint(1,5)
+    # selected_data = df.iloc[start_idx:start_idx + 100]
+    # print(start_idx, selected_data['ClassLabel'].values)
+    person_df = df[df['ClassLabel'] == target_idx]
+    for col in ['X', 'Y', 'Z', 'Mixed']:
+        person_df = remove_outliers_iqr(person_df, col)
 
-    metrics = []
-    for col in [y1, y2, y3, y4]:
-        window = col
+    
+    # Generate random data
+    # d = {'X': np.random.rand(100)*0.6-0.1, 
+    #      'Y': np.random.rand(100)*0.4-0.4, 
+    #      'Z': np.random.rand(100)*0.4-0.002, 
+    #      'Mixed': np.random.rand(100)}
+    d = {}
+
+    x = np.linspace(0, 100, 100)
+
+    
+    i = np.random.randint(0, 350)
+    metrics = defaultdict(list)
+    for col in ['X', 'Y', 'Z', 'Mixed']:
+        window = person_df[col].iloc[i:i + 100]
+        d[col] = window
         mean, std_dev, energy, entropy, num_peaks = calculate_metrics(window)
-        metrics.append(mean)
-        metrics.append(std_dev)
-        metrics.append(energy)
-        metrics.append(entropy)
-        metrics.append(num_peaks)
-    metrics = np.array(metrics)
+        metrics[f'Mean_{col}'].append(mean)
+        metrics[f'Std Dev_{col}'].append(std_dev)
+        metrics[f'Energy_{col}'].append(energy)
+        metrics[f'Entropy_{col}'].append(entropy)
+        metrics[f'Peaks_{col}'].append(num_peaks)
     input = pd.DataFrame(metrics)
     target = predict(input)
     print(target)
 
-    return jsonify({'x': x.tolist(), 'y1': y1, 'y2': y2, 'y3': y3, 'y4': y4, 'target': target.tolist()[0]})
+    return jsonify({'x': x.tolist(), 'y1': d['X'].tolist(), 'y2': d['Y'].tolist(), 'y3': d['Z'].tolist(), 
+                    'y4': d['Mixed'].tolist(), 'target': target[0]})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
--- Project/src/pipeline/predict_pipeline.py
+++ Project/src/pipeline/predict_pipeline.py
@@ -3,6 +3,7 @@ import os
 import pandas as pd
 from src.exception import CustomException
 from src.utils import load_object
+from src.logger import logging
 
 
 
@@ -10,16 +11,18 @@ from src.utils import load_object
 def predict(features):
     try:
         model_path=os.path.join("artifacts","model.pkl")
-        preprocessor_path=os.path.join('artifacts','preprocessor.pkl')
-        print("Before Loading")
+        preprocessor_path=os.path.join('artifacts','proprocessor.pkl')
+        logging.info("Loading models...")
         model=load_object(file_path=model_path)
         preprocessor=load_object(file_path=preprocessor_path)
-        print("After Loading")
+        logging.info("Loaded models")
         data_scaled=preprocessor.transform(features)
         preds=model.predict(data_scaled)
+        logging.info("Predictions made")
         return preds
     
     except Exception as e:
+        logging.error(e)
         raise CustomException(e,sys)
 
 
